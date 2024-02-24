d45b4053127949a3b8dd559b56e57de4c2eb94d3
Paul Pichon F <paul10_barca@hotmail.com>
2024-02-23 18:34:00 -0600

    Create: creacion de web-server con http2 y de forma manual

--- src/app.http.ts
+++ src/app.http.ts
@@ -1,10 +1,7 @@
 import fs from 'fs';
-import http2 from 'http2';
+import http from 'http';
 
-const server = http2.createSecureServer( {
-    key: fs.readFileSync('./keys/server.key'),
-    cert: fs.readFileSync('./keys/server.crt'),
-},( req, res ) => {
+const server = http.createServer(( req, res ) => {
     console.log( req.url );
 
     // res.writeHead( 200, {'Content-Type': 'text/html' } );
@@ -28,13 +25,9 @@ const server = http2.createSecureServer( {
         res.writeHead(200, { 'Content-Type': 'text/css' });
     }
 
-    try {
-        const responseContent = fs.readFileSync(`./public/${ req.url }`, 'utf-8');
-        res.end( responseContent );    
-    } catch (error) {
-        res.writeHead( 404, {'Content-Type': 'text/html' } );
-        res.end();
-    }
+    const responseContent = fs.readFileSync(`./public/${ req.url }`, 'utf-8');
+    res.end( responseContent );
+
 
 });
 
